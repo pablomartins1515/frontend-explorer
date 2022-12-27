d67600844421f21e2520c5deeaa250b02c784def
Pablo Martins <pablomartins1515@hotmail.com>
2022-12-26 23:39:08 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 export const api = axios.create({
-  //baseURL: "https://food-explorerr-api.herokuapp.com"
-  baseURL: "https://54.233.90.83:3333"
+  baseURL: "https://localhost:3333"
+  //baseURL: "https://54.233.90.83:3333"
 })
