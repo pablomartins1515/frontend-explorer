9943267af36d10713ad45b098fb23edda5dae8d6
Pablo Martins <pablomartins1515@hotmail.com>
2022-12-27 16:07:28 -0300

    Api online

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 export const api = axios.create({
-  baseURL: "https://localhost:3333"
-  //baseURL: "https://54.233.90.83:3333"
+
+  //baseURL: "http://localhost:3333"
+    baseURL: "http://15.229.68.5:3333"
+
 })
